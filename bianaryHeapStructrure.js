0e256f97e165979a791401e55811ccbf5fa791c5
brax3894 <42273897+brax3894@users.noreply.github.com>
2019-04-12 13:56:08 -0400

    data structures
    more data structures

---
+++ bianaryHeapStructrure.js
@@ -0,0 +1,54 @@
+class MaxBianaryHeap{
+    constructor(){
+        this.values = []
+    }
+    insert(element){
+        this.values.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while (idx > 0){
+            let parentIdx = math.floor((idx-1)/2);
+            let parent = this.values[parentIdx];
+            if (element <= parent) break;
+            this.values[parentIdx] = element ;
+                this.values[idx] = parent;
+                idx = parentIdx;
+        }
+    }
+    extractMax(){
+        const max = this.values[0];
+        const end = this.values.pop();
+        this.values[0] = end ;
+        this.sinkDown();
+        return max;
+    }
+    sinkDown(){
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        let leftChildIdx = 2*idx +1;
+        let rightChildIdx = 2*idx +2 ; 
+        let leftChild, rightChild ;
+        let swap = null;
+
+        if(leftChildIdx < length){
+            leftChild = this.values[leftChildIdx];
+            if (leftChild > element){
+                swap = leftChildIdx;
+            }
+        }
+
+        if(rightChildIdx < length){
+            rightChild = this.values[rightChildIdx];
+            if (
+                (swap===null && rightChild > element) || 
+            (swap !== null && rightChild>leftChild)) {
+                swap = rightChildIdx;
+            }
+        }
+        if (swap === null) break;
+    }
+}
